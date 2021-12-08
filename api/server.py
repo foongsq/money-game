da3e74c8647f1383b2add9c44a23361fc33cecf3
foongsiqi <edu.foongs@gmail.com>
2021-12-08 18:57:26 +0800

    Add addMoney endpoint

--- api/server.py
+++ api/server.py
@@ -19,13 +19,17 @@ except:
   print("Error - Cannot connect to db")
 
 ########################################
-# To add a new user into the database
+# To add a new user (with username, password, and 0 money, 
+# and empty inventory) into the database
+# TODO: Make sure username is not already in the database
 @app.route("/signup", methods=["POST"])
 def signup():
   try:
     user = {
       "username": request.form["username"], 
       "password": request.form["password"],
+      "money": 0,
+      "inventory": [],
     }
     dbResponse = db.users.insert_one(user)
     return Response(
@@ -41,17 +45,19 @@ def signup():
     print(ex)
 
 ########################################
-# To get a userid from the database
+# To get all user's data from the database
 @app.route("/signin", methods=["GET"])
 def signin():
   try:
     data = list(db.users.find({"username": request.form["username"]}))
-    print(data)
     return Response(
       response=json.dumps( # send object as a json
         {
           "message": "User succesfully retrieved", 
           "username": data[0]["username"],
+          "password": data[0]["password"],
+          "money": data[0]["money"],
+          "inventory": data[0]["inventory"],
         }
       ), 
       status=200, 
@@ -62,6 +68,36 @@ def signin():
       response=json.dumps( # send object as a json
         {
           "message": "User cannot be retrieved", 
+        }
+      ), 
+      status=500, 
+      mimetype="application/json")
+
+########################################
+# To get a userid from the database
+@app.route("/addMoney", methods=["POST"])
+def addMoney():
+  try:
+    dbResponse = db.users.update_one(
+      {"username": request.form["username"]},
+      {"$set": { "money": request.form["money"]}}
+    )
+    # for attr in dir(dbResponse):
+    #   print(attr)
+    return Response(
+      response=json.dumps( # send object as a json
+        {
+          "message": "Money succesfully updated", 
+        }
+      ), 
+      status=200, 
+      mimetype="application/json")
+  except Exception as ex:
+    print(ex)
+    return Response(
+      response=json.dumps( # send object as a json
+        {
+          "message": "Money cannot be updated", 
         }
       ), 
       status=500, 
