d97809a2d75fa6ca0691d37b240facad70bc8356
foongsiqi <edu.foongs@gmail.com>
2022-01-04 14:44:33 +0800

    Set up mongodb hosting

--- api/routes/database.py
+++ api/routes/database.py
@@ -1,9 +1,15 @@
 import pymongo # To connect flask app to mongodb
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGODB_PASS = os.getenv('MONGODB_PASS')
 
 # Connect to mongodb (always wrap connection in try except block)
 try:
   mongo = pymongo.MongoClient(
-    host="localhost", 
+    host=f"mongodb+srv://moneygame-admin:{MONGODB_PASS}@moneygame-db.2gc9y.mongodb.net/moneygameDB?retryWrites=true&w=majority", 
     port=27017, # default TCP port used by mongodb
     serverSelectionTimeoutMS=1000 # if cannot connect in 1000ms, means connection failed
   )
