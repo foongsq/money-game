677eb0635e9cdb25f2ef7ab68ede27f76f4328e2
siqi <edu.foongs@gmail.com>
2022-10-16 13:49:54 +0800

    Update env for backend

--- api/database.py
+++ api/database.py
@@ -1,16 +1,17 @@
-import pymongo # To connect flask app to mongodb
+import pymongo  # To connect flask app to mongodb
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 MONGODB_URI = os.getenv('MONGODB_URI')
-DB_NAME = "moneygame-db"
+DB_NAME = os.getenv('MONGODB_DB')
 
 # Connect to mongodb (always wrap connection in try except block)
 try:
-  mongo = pymongo.MongoClient(MONGODB_URI)
-  db = mongo.moneygame # access database named moneygame
-  mongo.server_info() # trigger exception if connection fails
-except: 
-  print("Error - Cannot connect to db") 
+    mongo = pymongo.MongoClient(MONGODB_URI)
+    db = mongo[DB_NAME]
+    mongo.server_info()  # trigger exception if connection fails
+    print("Connected to db!")
+except:
+    print("Error - Cannot connect to db")
